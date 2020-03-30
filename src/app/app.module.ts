b08fc873f372c0befa2d3df0a7c8bc0eda1df281
Maksym Maiboroda <crashdey323@gmail.com>
2020-03-30 22:46:45 +0300

    selectorComponent_done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {SelectorComponent} from './selector/selector.component';
+import {OrganizerComponent} from './organizer/organizer.component';
+import {MomentPipe} from './shared/moment.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    SelectorComponent,
+    OrganizerComponent,
+    MomentPipe
   ],
   imports: [
     BrowserModule
@@ -13,4 +21,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
